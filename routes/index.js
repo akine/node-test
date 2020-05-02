d3b890855a2c133e2cad470708231d5152e05f09
akine <weezer.and.you@gmail.com>
2020-05-02 09:33:20 +0900

    fix index

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var connection = require('../mysqlConnection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS create_at FROM boards';
+  connection.query(query, function(err, rows) {
+    console.log(rows);
+    res.render('index', { title: 'Express' });
+  });
 });
 
 router.post('/', function(req, res, next) {
