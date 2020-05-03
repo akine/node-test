c406023b921b2128e14e65dffab0531eacda8058
akine <weezer.and.you@gmail.com>
2020-05-03 14:52:40 +0900

    fix app.js

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session( {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
